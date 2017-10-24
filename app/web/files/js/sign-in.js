906dbf0f742d3fc778e882e70d8cb80ad6333a2e
unknown <>
2017-10-25 00:00:32 +0900

    http, socket.io api 테스트용 클라이언트 html 파일 추가

---
+++ app/web/files/js/sign-in.js
@@ -0,0 +1,30 @@
+const signInForm = document.getElementById("sign-in-form");
+
+signInForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const { id, password } = e.target.elements;
+    
+    signIn(id, password, (status) => {
+        if (status === 200) {
+            location.href = "/";
+        } else {
+            alert("Cannot match id or password");
+        }
+    });
+});
+
+function signIn(id, password, callback) {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = (e) => {
+        if (e.target.readyState === 4) {
+            callback(e.target.status);
+        }
+    };
+
+    xhr.open("POST", "http://localhost:5000/api/auth/signin", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.send(`id=${ id.value }&password=${ password.value }`);
+}
